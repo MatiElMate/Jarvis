2b264e6240405e0cbb4dbbd266770fcd65144f80
Matías Sposetti <matiassposetti60@gmail.com>
2026-08-07 11:59:16 -0300

    Feat: Factory ready to go

--- src/os_engine/factory.py
+++ src/os_engine/factory.py
@@ -1,6 +1,7 @@
 import platform
-import os
+import logging
 
+logger = logging.getLogger(__name__)
 
 _adapter_instance = None
 
@@ -12,7 +13,7 @@ def reset_adapter():
 
 def get_adapter():
     """Detecta el sistema operativo y retorna la instancia del adaptador correspondiente."""
-    global _adapter_instance
+    global _adapter_instance    
 
     if _adapter_instance is not None:
         return _adapter_instance
@@ -20,14 +21,18 @@ def get_adapter():
     platform_os = platform.system().lower()
 
     if platform_os == "windows":
+        logger.info(f"[OSfactory] Instanciando WindowsAdapter...")
         from src.os_engine.windows_adapter import WindowsAdapter
 
         _adapter_instance = WindowsAdapter()
     elif platform_os == "linux":
+        logger.info(f"[OSfactory] Instanciando LinuxAdapter...")
+
         from src.os_engine.linux_adapter import LinuxAdapter
 
         _adapter_instance = LinuxAdapter()
     else:
+        logger.critical(f"[OSfactory] Sistema operativo {platform_os} no soportado")
         raise NotImplementedError(f"El sistema operativo '{platform_os}' no está soportado actualmente")
 
     return _adapter_instance
