c614b940a9f38521fab91c83e93ebc7cdd9e173d
Matías Sposetti <matiassposetti60@gmail.com>
2026-08-08 15:34:37 -0300

    Feat: linux_finder listo

---
+++ src/app_finder/linux_finder.py
@@ -0,0 +1,75 @@
+from numpy.core.defchararray import startswith
+from genericpath import exists
+from re import sub
+import shlex
+import subprocess
+import logging
+from pathlib import Path
+from src.app_finder.base_finder import BaseAppFinder
+
+logger = logging.getLogger(__name__)
+
+
+class LinuxAppFinder(BaseAppFinder):
+
+    def __init__(self):
+        self._home = Path.home()
+        
+        self._desktop_paths = [
+            Path("/usr/share/applications"),
+            Path("/usr/local/share/applications"),
+            self._home / ".local/share/applications"  # Usamos pathlib para unir rutas
+        ]
+
+    @property
+    def desktop_paths(self) -> list[Path]:
+        return self._desktop_paths
+
+    
+    def _parse_desktop_file(file_path) -> tuple[str|None, str|None]:
+        name = None
+        command = None
+        hidden = False
+
+        with open(file_path, "r", encoding="utf-8") as file:
+            for line in file:
+                line = line.strip()
+            
+            if line.startswith("Name=") and name is None:
+                name = line.split("=", 1)[1]
+
+            elif line.startswith("Excec=") and command is None:
+                brute_command = line.strip("=",1)[1]
+                command = brute_command.split("%")[0].strip().replace('"',"")
+            
+            elif line.startswith("NoDisplay=true"):
+                hidden = True
+        
+        if hidden:
+            return (None,None)
+        else:
+            return (name,command)
+
+    def get_apps(self):
+        apps = {}
+
+        for folder in self.desktop_paths: 
+            if not(folder.exists()):
+                continue
+            
+            for file in folder.glob("*.desktop"):
+                try:
+                    name,command = self._parse_desktop_file(file)
+                    if name and command:
+                        apps[name.lower()] = command
+                        
+                except Exception as e:
+                    logger.debug(
+                        f"[LinuxFinder] Error leyendo {file} : {e}"
+                        )
+        
+        
+        logger.info(
+            f"[LinuxFinder] Se encontraron {len(apps)} aplicaciones en Linux"
+            )
+        return apps
